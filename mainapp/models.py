6e71053f31fe478c3771a85262dc262aa45d4a10
willymwaura <wilsonmwaura697@gmail.com>
2024-03-12 22:44:09 +0300

    added app label in all the models

--- mainapp/models.py
+++ mainapp/models.py
@@ -20,12 +20,17 @@ class User(models.Model):
 class CreditBalance(models.Model):
     user_id = models.IntegerField(unique=True)
     balance = models.IntegerField(default=0)
+
+    class Meta:
+        app_label = 'mainapp'
     def __str__(self):
         return f"User ID: {self.user_id}, Balance: {self.balance}"
 
 class ImageHistory(models.Model):
     user_id = models.IntegerField()
     image_url = models.URLField()
+    class Meta:
+        app_label = 'mainapp'
     def __str__(self):
         return self.image_url
 
@@ -33,5 +38,8 @@ class PaymentsActivated(models.Model):
     email=models.EmailField()
     phone=models.IntegerField()
     amount=models.IntegerField()
+
+    class Meta:
+        app_label = 'mainapp'
 
 
